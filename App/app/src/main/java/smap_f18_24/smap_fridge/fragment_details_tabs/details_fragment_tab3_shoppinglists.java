00ec7d7cd25e329c2c8986d15c4a30c4c5cfcfe9
Philip Nygaard Schmidt <phschmidt@grundfos.com>
2018-05-01 16:08:56 +0200

    The tab classes replaces the PlaceHolderFragment class - there deleted

--- App/app/src/main/java/smap_f18_24/smap_fridge/fragment_details_tabs/details_fragment_tab3_shoppinglists.java
+++ App/app/src/main/java/smap_f18_24/smap_fridge/fragment_details_tabs/details_fragment_tab3_shoppinglists.java
@@ -1,4 +1,33 @@
 package smap_f18_24.smap_fridge.fragment_details_tabs;
 
-public class details_fragment_tab3_shoppinglists {
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import smap_f18_24.smap_fridge.R;
+
+public class details_fragment_tab3_shoppinglists extends Fragment {
+
+    @Override
+    public View onCreate(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_details_tab3_shoppinglists, container, false);
+        return rootView;
+
+    }
 }
