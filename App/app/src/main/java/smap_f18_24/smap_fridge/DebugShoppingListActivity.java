52d85ed29ffd95fccbdbd1bc56203778a7001709
Dalle <Daniel.toettrup@gmail.com>
2018-05-05 16:46:59 +0200

    Ingredientslistadaptor funger

--- App/app/src/main/java/smap_f18_24/smap_fridge/DebugShoppingListActivity.java
+++ App/app/src/main/java/smap_f18_24/smap_fridge/DebugShoppingListActivity.java
@@ -9,9 +9,11 @@ import java.util.ArrayList;
 
 import smap_f18_24.smap_fridge.Adaptors.EssentialsListAdaptor;
 import smap_f18_24.smap_fridge.Adaptors.FridgeListAdaptor;
+import smap_f18_24.smap_fridge.Adaptors.IngredientsListAdaptor;
 import smap_f18_24.smap_fridge.Adaptors.InventoryListAdaptor;
 import smap_f18_24.smap_fridge.Adaptors.ShoppingListAdaptor;
 import smap_f18_24.smap_fridge.ModelClasses.EssentialsList;
+import smap_f18_24.smap_fridge.ModelClasses.IngredientList;
 import smap_f18_24.smap_fridge.ModelClasses.InventoryList;
 import smap_f18_24.smap_fridge.ModelClasses.Item;
 import smap_f18_24.smap_fridge.ModelClasses.ShoppingList;
@@ -22,9 +24,11 @@ public class DebugShoppingListActivity extends AppCompatActivity {
     final public ArrayList<Item>  debugList = new ArrayList<>();
     final public EssentialsList essentialList = new EssentialsList();
     final public InventoryList inventoryList = new InventoryList();
+    final public ArrayList<Item>  debugList1 = new ArrayList<>();
     public ShoppingListAdaptor adaptor1 = new ShoppingListAdaptor(this, debugList);
     public EssentialsListAdaptor adaptor2 = new EssentialsListAdaptor(this, essentialList);
     public InventoryListAdaptor adaptor3 = new InventoryListAdaptor(this, inventoryList);
+    public IngredientsListAdaptor adaptor4 = new IngredientsListAdaptor(this, debugList1);
 
     Item kartoffel = new Item("katoffel", "kg", 1000, "hejmeddig123@dibidut.au", "Status");
     Item Tomat = new Item("Tomat", "kg", 100, "hejmeddig123@dibidut.au", "Status");
@@ -53,12 +57,17 @@ public class DebugShoppingListActivity extends AppCompatActivity {
         inventoryList.AddItem(Æg);
         inventoryList.AddItem(juice);
 
+        debugList1.add(kartoffel);
+        debugList1.add(Tomat);
+        debugList1.add(Æg);
+        debugList1.add(juice);
+
 
        Lv_Shoppinglist = findViewById(R.id.debug_Lv_Shoppinglist);
 
        //Lv_Shoppinglist.setAdapter(adaptor1);
 
-       Lv_Shoppinglist.setAdapter(adaptor3);
+       Lv_Shoppinglist.setAdapter(adaptor4);
 
 
     }
