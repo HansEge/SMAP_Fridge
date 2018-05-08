6b6ec19afa6e6f4e69bfc59d0ea54689c8c3dbf9
Dalle <Daniel.toettrup@gmail.com>
2018-05-08 13:12:32 +0200

    indsat listview i inventoryfragment

--- App/app/src/main/java/smap_f18_24/smap_fridge/fragment_details_tabs/details_fragment_tab1_inventory.java
+++ App/app/src/main/java/smap_f18_24/smap_fridge/fragment_details_tabs/details_fragment_tab1_inventory.java
@@ -1,36 +1,50 @@
 package smap_f18_24.smap_fridge.fragment_details_tabs;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import smap_f18_24.smap_fridge.OverviewActivity;
+import java.util.ArrayList;
+import java.util.List;
+
+import smap_f18_24.smap_fridge.Adaptors.InventoryListAdaptor;
+import smap_f18_24.smap_fridge.ModelClasses.EssentialsList;
+import smap_f18_24.smap_fridge.ModelClasses.Fridge;
+import smap_f18_24.smap_fridge.ModelClasses.IngredientList;
+import smap_f18_24.smap_fridge.ModelClasses.InventoryList;
+import smap_f18_24.smap_fridge.ModelClasses.Item;
+import smap_f18_24.smap_fridge.ModelClasses.ShoppingList;
 import smap_f18_24.smap_fridge.R;
 
 
 public class details_fragment_tab1_inventory extends Fragment {
 
-    public TextView test;
+
     public Button btn_goBackToOverview;
+
+
+    private Fridge fridge;
+
+    List<String> connectedUserEmailss;
+    InventoryList inventoryList = new InventoryList();
+    EssentialsList essentialList = new EssentialsList();
+
+    List<ShoppingList> myShoppingLists = new ArrayList<ShoppingList>();
+    List<IngredientList> myIngredientsLists = new ArrayList<IngredientList>();
+
+    Item kartoffel = new Item("katoffel", "kg", 1000, "hejmeddig123@dibidut.au", "Status");
+    Item Tomat = new Item("Tomat", "kg", 100, "hejmeddig123@dibidut.au", "Status");
+    Item Æg = new Item("Æg", "stk", 10, "hejmeddig123@dibidut.au", "Status");
+    Item juice = new Item("Juice", "L", 2, "hejmeddig123@dibidut.au", "Status");
 
     public String clickedFridgeID;
 
@@ -40,14 +54,26 @@ public class details_fragment_tab1_inventory extends Fragment {
         View v = inflater.inflate(R.layout.fragment_details_tab1_inventory, container, false);
 
         // INITIALIZING
-        test = (TextView) v.findViewById(R.id.details_tab1_inventory_tv_sectionLabel);
+
         btn_goBackToOverview = (Button) v.findViewById(R.id.details_tab1_inventory_btn_backToOverView);
+        ListView lv_inventoryList = v.findViewById(R.id.lv_inventoryList_tap1);
+
 
         final SharedPreferences sharedData = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
         clickedFridgeID = sharedData.getString("clickedFridgeID","errorNoValue");
 
 
+        //fridge = ((DetailsActivity)getActivity()).mService.getFridge(((DetailsActivity)getActivity()).clickedFridgeID); //TODO fix ID
+        //fridge = new Fridge("Tester", "testID", connectedUserEmailss, inventoryList, essentialList, myShoppingLists, myIngredientsLists);
 
+        inventoryList.AddItem(kartoffel);
+        inventoryList.AddItem(Tomat);
+        inventoryList.AddItem(Æg);
+        inventoryList.AddItem(juice);
+
+        InventoryListAdaptor inventoryListAdaptor = new InventoryListAdaptor(getActivity().getApplicationContext(),inventoryList);
+
+        lv_inventoryList.setAdapter(inventoryListAdaptor);
 
 
         btn_goBackToOverview.setOnClickListener(new View.OnClickListener() {
